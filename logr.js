d4405794d004b3b48deaf8dfd4d545dd807e67c7
Daniel Santana <daniel@everymundo.com>
2016-02-03 16:17:07 -0500

    logr

---
+++ logr.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const bunyan = require('bunyan');
+
+const level  = process.env.LOG_LEVEL || 'debug';
+
+const logr = bunyan.createLogger({
+    name: 'LOGR',
+    level,
+    serializers: {
+      req: bunyan.stdSerializers.req,
+      res: bunyan.stdSerializers.res
+    },
+    src: (level === 'debug')
+});
+
+module.exports = logr;
